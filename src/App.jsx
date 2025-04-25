416d3f45df44aeb8bfaa8323d06ee35740084080
Timothy McDermott <162518123+timwebdev1@users.noreply.github.com>
2025-04-24 22:04:29 -0500

    fix: corrected invalid import path for Login component

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,20 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Home from './pages/home/Home';
 import About from './pages/about/About';
-import Login from '.components/Login/Login';
-import Register from '.components/Register/Register';
+import Login from './components/Login/Login';
+import Register from './components/Register/Register';
 import Profile from './pages/profile/Profile';
-import Transaction from '.components/Transaction/Transaction'
-import IncomeSplit from '.components/IncomeSplit/IncomeSplit'
+import Transaction from './components/Transaction/Transaction'
+import IncomeSplit from './components/IncomeSplit/IncomeSplit'
 import Contact from './pages/contact/Contact';
-import TransactionSearch from '.components/TransactionSearch/TransactionSearch';
+import TransactionSearch from './components/TransactionSearch/TransactionSearch';
 
 
-import NavHeader from '.components/navHeader/NavHeader';
-import Footer from '.components/footer/Footer';
+import NavHeader from './components/navHeader/NavHeader';
+import Footer from './components/footer/Footer';
 
 import { AuthProvider } from './context/AuthContext';
-import CreateBudget from '.components/CreateBudget/CreateBudget';
+import CreateBudget from './components/CreateBudget/CreateBudget';
 
 function App() {
   return (
