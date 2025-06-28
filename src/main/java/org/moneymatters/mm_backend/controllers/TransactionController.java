de075bad4372cb4ffaff816de6782a346564ca7c
Timothy McDermott <162518123+timwebdev1@users.noreply.github.com>
2025-06-28 16:44:35 -0500

    removed trailing slashes on urls

--- src/main/java/org/moneymatters/mm_backend/controllers/TransactionController.java
+++ src/main/java/org/moneymatters/mm_backend/controllers/TransactionController.java
@@ -21,7 +21,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/transactions")
-@CrossOrigin(origins = {"http://localhost:3000", "https://mybudgetapp-fe.netlify.app/"}, allowCredentials = "true")
+@CrossOrigin(origins = {"http://localhost:3000", "https://mybudgetapp-fe.netlify.app"}, allowCredentials = "true")
 public class TransactionController {
 
     @Autowired
